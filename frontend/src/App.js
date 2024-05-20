3d250471042c7da7c719d0c48b2e598b9044a0f0
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 13:48:08 +0700

    Modify App 🫸🌀✏️📗🐧🐳

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-}
+    const modalRef = useRef();
+    const fileRef = useRef();
+    const [data, setData] = useState({});
+    const [currentPage, setCurrentPage] = useState(0);
+    const [file, setFile] = useState(undefined);
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        address: '',
+        title: '',
+        status: '',
+    });
 
 export default App;
