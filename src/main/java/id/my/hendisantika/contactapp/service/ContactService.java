38b598eb11344d1b49f641fcf7a42b2ad0d93e9b
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 11:54:43 +0700

    Add CorsConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/contactapp/service/ContactService.java
+++ src/main/java/id/my/hendisantika/contactapp/service/ContactService.java
@@ -2,17 +2,25 @@ package id.my.hendisantika.contactapp.service;
 
 import id.my.hendisantika.contactapp.entity.Contact;
 import id.my.hendisantika.contactapp.repository.ContactRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
+import java.util.function.BiFunction;
 import java.util.function.Function;
+
+import static id.my.hendisantika.contactapp.entity.Constant.PHOTO_DIRECTORY;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 /**
  * Created by IntelliJ IDEA.
@@ -59,4 +67,20 @@ public class ContactService {
 
     private final Function<String, String> fileExtension = filename -> Optional.of(filename).filter(name -> name.contains("."))
             .map(name -> "." + name.substring(filename.lastIndexOf(".") + 1)).orElse(".png");
+
+    private final BiFunction<String, MultipartFile, String> photoFunction = (id, image) -> {
+        String filename = id + fileExtension.apply(image.getOriginalFilename());
+        try {
+            Path fileStorageLocation = Paths.get(PHOTO_DIRECTORY).toAbsolutePath().normalize();
+            if (!Files.exists(fileStorageLocation)) {
+                Files.createDirectories(fileStorageLocation);
+            }
+            Files.copy(image.getInputStream(), fileStorageLocation.resolve(filename), REPLACE_EXISTING);
+            return ServletUriComponentsBuilder
+                    .fromCurrentContextPath()
+                    .path("/contacts/image/" + filename).toUriString();
+        } catch (Exception exception) {
+            throw new RuntimeException("Unable to save image");
+        }
+    };
 }
