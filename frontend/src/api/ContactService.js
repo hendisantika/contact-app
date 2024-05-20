8cd516e29b32f9005c0e6f8f94849ad38e7aacf9
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 13:18:24 +0700

    Add getContact in ContactService.js🫸🌀✏️📗🐧🐳

--- frontend/src/api/ContactService.js
+++ frontend/src/api/ContactService.js
@@ -10,3 +10,7 @@ export async function getContacts(page = 0, size = 10) {
     return await axios.get(`${API_URL}?page=${page}&size=${size}`);
 }
 
+export async function getContact(id) {
+    return await axios.get(`${API_URL}/${id}`);
+}
+
