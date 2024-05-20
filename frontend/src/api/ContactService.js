4ac5af0811bd70fc9e7b1661ea4c261631d7247a
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 13:18:58 +0700

    Add updatePhoto in ContactService.js🫸🌀✏️📗🐧🐳

--- frontend/src/api/ContactService.js
+++ frontend/src/api/ContactService.js
@@ -17,3 +17,7 @@ export async function getContact(id) {
 export async function updateContact(contact) {
     return await axios.post(API_URL, contact);
 }
+
+export async function updatePhoto(formData) {
+    return await axios.put(`${API_URL}/photo`, formData);
+}
