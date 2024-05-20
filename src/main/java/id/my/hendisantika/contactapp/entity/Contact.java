fa20ca8516c755c94121a783620398aec4785760
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 11:48:26 +0700

    Add Contact Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/contactapp/entity/Contact.java
@@ -0,0 +1,46 @@
+package id.my.hendisantika.contactapp.entity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_DEFAULT;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : contact-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/20/24
+ * Time: 11:47
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(NON_DEFAULT)
+@Table(name = "contacts")
+public class Contact {
+    @Id
+    @UuidGenerator
+    @Column(name = "id", unique = true, updatable = false)
+    private String id;
+    private String name;
+    private String email;
+    private String title;
+    private String phone;
+    private String address;
+    private String status;
+    private String photoUrl;
+}
