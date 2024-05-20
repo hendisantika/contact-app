44943468fa097a95a0e91ee263d1d50b81c3cb00
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 13:51:02 +0700

    Modify App 🫸🌀✏️📗🐧🐳

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,12 @@
 import './App.css';
+import {ToastContainer} from "react-toastify";
+import Header from "./components/Header";
+import {Navigate, Route, Routes} from "react-router-dom";
+import React, {useEffect, useRef, useState} from "react";
+import ContactList from "./components/ContactList";
+import ContactDetail from "./components/ContactDetail";
+import {toastError} from "./api/ToastService";
+import {getContacts, saveContact} from "./api/ContactService";
 
 function App() {
     const modalRef = useRef();
@@ -75,5 +83,79 @@ function App() {
             toastError(error.message);
         }
     };
+
+    const toggleModal = show => show ? modalRef.current.showModal() : modalRef.current.close();
+
+    useEffect(() => {
+        getAllContacts();
+    }, []);
+
+    return (
+        <>
+            <Header toggleModal={toggleModal} nbOfContacts={data.totalElements}/>
+            <main className='main'>
+                <div className='container'>
+                    <Routes>
+                        <Route path='/' element={<Navigate to={'/contacts'}/>}/>
+                        <Route path="/contacts" element={<ContactList data={data} currentPage={currentPage}
+                                                                      getAllContacts={getAllContacts}/>}/>
+                        <Route path="/contacts/:id"
+                               element={<ContactDetail updateContact={updateContact} updateImage={updateImage}/>}/>
+                    </Routes>
+                </div>
+            </main>
+
+            {/* Modal */}
+            <dialog ref={modalRef} className="modal" id="modal">
+                <div className="modal__header">
+                    <h3>New Contact</h3>
+                    <i onClick={() => toggleModal(false)} className="bi bi-x-lg"></i>
+                </div>
+                <div className="divider"></div>
+                <div className="modal__body">
+                    <form onSubmit={handleNewContact}>
+                        <div className="user-details">
+                            <div className="input-box">
+                                <span className="details">Name</span>
+                                <input type="text" value={values.name} onChange={onChange} name='name' required/>
+                            </div>
+                            <div className="input-box">
+                                <span className="details">Email</span>
+                                <input type="text" value={values.email} onChange={onChange} name='email' required/>
+                            </div>
+                            <div className="input-box">
+                                <span className="details">Title</span>
+                                <input type="text" value={values.title} onChange={onChange} name='title' required/>
+                            </div>
+                            <div className="input-box">
+                                <span className="details">Phone Number</span>
+                                <input type="text" value={values.phone} onChange={onChange} name='phone' required/>
+                            </div>
+                            <div className="input-box">
+                                <span className="details">Address</span>
+                                <input type="text" value={values.address} onChange={onChange} name='address' required/>
+                            </div>
+                            <div className="input-box">
+                                <span className="details">Account Status</span>
+                                <input type="text" value={values.status} onChange={onChange} name='status' required/>
+                            </div>
+                            <div className="file-input">
+                                <span className="details">Profile Photo</span>
+                                <input type="file" onChange={(event) => setFile(event.target.files[0])} ref={fileRef}
+                                       name='photo' required/>
+                            </div>
+                        </div>
+                        <div className="form_footer">
+                            <button onClick={() => toggleModal(false)} type='button' className="btn btn-danger">Cancel
+                            </button>
+                            <button type='submit' className="btn">Save</button>
+                        </div>
+                    </form>
+                </div>
+            </dialog>
+            <ToastContainer/>
+        </>
+    );
+}
 
     export default App;
