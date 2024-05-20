b9d8c0c2d51a5c80753174bf388e0b7d6076fed2
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 11:56:09 +0700

    Add getContacts in ContactController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/contactapp/controller/ContactController.java
+++ src/main/java/id/my/hendisantika/contactapp/controller/ContactController.java
@@ -3,10 +3,13 @@ package id.my.hendisantika.contactapp.controller;
 import id.my.hendisantika.contactapp.entity.Contact;
 import id.my.hendisantika.contactapp.service.ContactService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -33,4 +36,9 @@ public class ContactController {
         return ResponseEntity.created(URI.create("/contacts/userID")).body(contactService.createContact(contact));
     }
 
+    @GetMapping
+    public ResponseEntity<Page<Contact>> getContacts(@RequestParam(value = "page", defaultValue = "0") int page,
+                                                     @RequestParam(value = "size", defaultValue = "10") int size) {
+        return ResponseEntity.ok().body(contactService.getAllContacts(page, size));
+    }
 }
