e2965e461cabb720b6265b8beddd965b3f9105c9
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 11:55:59 +0700

    Add createContact in ContactController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/contactapp/controller/ContactController.java
+++ src/main/java/id/my/hendisantika/contactapp/controller/ContactController.java
@@ -1,9 +1,15 @@
 package id.my.hendisantika.contactapp.controller;
 
+import id.my.hendisantika.contactapp.entity.Contact;
 import id.my.hendisantika.contactapp.service.ContactService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +26,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ContactController {
     private final ContactService contactService;
+
+    @PostMapping
+    public ResponseEntity<Contact> createContact(@RequestBody Contact contact) {
+        //return ResponseEntity.ok().body(contactService.createContact(contact));
+        return ResponseEntity.created(URI.create("/contacts/userID")).body(contactService.createContact(contact));
+    }
+
 }
