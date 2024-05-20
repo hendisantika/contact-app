853668d8c32adf2de434a88221377d6075fd41b2
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 11:56:28 +0700

    Add uploadPhoto in ContactController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/contactapp/controller/ContactController.java
+++ src/main/java/id/my/hendisantika/contactapp/controller/ContactController.java
@@ -8,10 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URI;
 
@@ -48,4 +50,8 @@ public class ContactController {
         return ResponseEntity.ok().body(contactService.getContact(id));
     }
 
+    @PutMapping("/photo")
+    public ResponseEntity<String> uploadPhoto(@RequestParam("id") String id, @RequestParam("file") MultipartFile file) {
+        return ResponseEntity.ok().body(contactService.uploadPhoto(id, file));
+    }
 }
