14d11a82fb6ff156b62d9462afae89e3c4396507
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 11:49:39 +0700

    Add Constant Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/contactapp/entity/Constant.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.contactapp.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : contact-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/20/24
+ * Time: 11:49
+ * To change this template use File | Settings | File Templates.
+ */
+public class Constant {
+    public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
+
+    public static final String X_REQUESTED_WITH = "X-Requested-With";
+}
