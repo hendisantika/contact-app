e45992a9533ec2887e5050963de0b9b876462149
hendisantika <hendisantika@yahoo.co.id>
2024-05-20 13:19:37 +0700

    Add ToastService.js🫸🌀✏️📗🐧🐳

---
+++ frontend/src/api/ToastService.js
@@ -0,0 +1,12 @@
+import {toast} from "react-toastify";
+
+const toastConfig = {
+    position: toast.POSITION.TOP_RIGHT,
+    autoClose: 1500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light"
+};
